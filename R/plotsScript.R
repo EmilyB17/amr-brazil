978dcdb6b65a82ec3f11c037112d124c97cb118f
Emily <epb5360@psu.edu>
2020-03-30 12:17:29 -0400

    updated plots w/normalized relative abundance

--- R/plotsScript.R
+++ R/plotsScript.R
@@ -1,4 +1,4 @@
-## Making Plots for USDA Grant
+## Making Plots for USDA Grant - updated 3/20/20 with relative abundance data
 
 require(dplyr)
 require(tidyr)
@@ -9,52 +9,77 @@ require(ggplot2)
 theme_set(theme_bw())
 
 # read in data
-counts <- read.table("https://github.com/EmilyB17/amr-brazil/blob/master/data/parsedGeneCounts.txt?raw=true", sep = "\t", header = TRUE)
+counts <- read.table("https://github.com/EmilyB17/amr-brazil/blob/master/data/parsedRelativeAbundance.txt?raw=true", sep = "\t", header = TRUE)
+
+# get list of controls
+controls <- paste("G527_94_NoTemplate-DNAextraction2_S94",
+                  "G527_96_NoTemplate-LibraryPrep_S96",
+                  "G527_95_MockCommunity_S95",
+                  "G527_93_NoTemplate-DNAextraction1_S93",
+                  "G527_49_2019_DNA_S49", sep = "|")
+
+# remove controls
+nocont <- counts %>% 
+  mutate(name = as.character(name)) %>% 
+  filter(!str_detect(name, controls))
 
 # summarize data for each gene and make wider by farm
-sumfarm <- counts %>% 
+sumfarm <- nocont %>% 
   select(-nhits) %>% 
   group_by(class, type, protein, gene, farm, site) %>% 
   summarize(count = sum(count))  %>% 
   ungroup() %>% 
-  pivot_wider(names_from = farm, values_from = count, names_prefix = "farm_", values_fill = list(count = 0))
+  pivot_wider(names_from = farm, values_from = count, names_prefix = "farm_", values_fill = list(count = 5))
 
-# make vertical and add nicer column names
-sumv <- sumfarm %>% 
-  pivot_longer(cols = starts_with("farm_"), names_to = "farm", names_prefix = "farm_", values_to = "count") %>% 
-  mutate(logcount = log1p(count)) %>% 
+# add nicer column names
+sumv <- nocont %>% 
+  
   # fix names of everything for plots
   mutate(`Resistance Type` = gsub("_", " ", protein),
          `Resistance Class` = gsub("_", " ", type),
          `General Type` = gsub("_", " ", class)) %>% 
-  rename(Gene = gene) %>% 
-  # add log transformation of gene counts
-  mutate(logcount = log1p(count))
+  rename(Gene = gene) 
 
 ## ---- Plots ----
 
 ## BARPLOT
 # multi drug resistance for farms, body sites
 ggplot(data = filter(sumv, type == "Multi-drug_resistance"), 
-       aes(x = farm, y = logcount, color = `Resistance Type`, fill = `Resistance Type`)) +
+       aes(x = farm, y = relabun, color = `Resistance Type`, fill = `Resistance Type`
+           )) +
   geom_col() +
-  labs(x = "Farm", y = "log(Number of genes)", title = "Multi-drug resistance") +
+  labs(x = "Farm", y = "Relative Abundance", title = "Multi-drug resistance") +
   coord_flip()
+# save
+ggsave("./data/plots/relabun-barplot-farm.png", plot = last_plot(),
+       width = 6, height = 5, units = "in")
 # body site
 ggplot(data = filter(sumv, type == "Multi-drug_resistance"), 
-       aes(x = site, y = logcount, color = `Resistance Type`, fill = `Resistance Type`)) +
+       aes(x = site, y = relabun, color = `Resistance Type`, fill = `Resistance Type`)) +
   geom_col() +
-  labs(x = "Body Site", y = "log(Number of genes)", title = "Multi-drug resistance") +
+  labs(x = "Body Site", y = "Relative Abundance", title = "Multi-drug resistance") +
   coord_flip()
+# save
+ggsave("./data/plots/relabun-barplot-body-site.png", plot = last_plot(),
+       width = 6, height = 5, units = "in")
 
 ## BOXPLOT
 # multi drug resistance for farms, body sites
 ggplot(data = filter(sumv, type == "Multi-drug_resistance"), 
-       aes(x = farm, y = logcount, color = `Resistance Type`, fill = `Resistance Type`)) +
+       aes(x = farm, y = relabun, color = `Resistance Type`, fill = `Resistance Type`)) +
   geom_boxplot() +
-  labs(x = "Farm", y = "log(Number of genes)", title = "Multi-drug resistance")
+  labs(x = "Farm", y = "Relative Abundance", title = "Multi-drug resistance")
+
+# save
+ggsave("./data/plots/relabun-boxplot-farm.png", plot = last_plot(),
+       width = 6, height = 5, units = "in")
+
 # body site
 ggplot(data = filter(sumv, type == "Multi-drug_resistance"), 
-       aes(x = site, y = logcount, color = `Resistance Type`, fill = `Resistance Type`)) +
+       aes(x = site, y = relabun, color = `Resistance Type`, fill = `Resistance Type`)) +
   geom_boxplot() +
-  labs(x = "Body Site", y = "log(Number of genes)", title = "Multi-drug resistance")
+  labs(x = "Body Site", y = "Relative Abundance", title = "Multi-drug resistance")
+
+# save
+ggsave("./data/plots/relabun-boxplot-body-site.png", plot = last_plot(),
+       width = 6, height = 5, units = "in")
