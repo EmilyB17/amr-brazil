e14c733fb0e96d3e5cb7809e971324e41128e38f
Emily Bean <epb5360@psu.edu>
2020-04-14 10:02:36 -0400

    working

--- R/workingScript.R
+++ R/workingScript.R
@@ -1,60 +1,52 @@
-## post-decontam normalization
+# stat analysis working
 
-## get the gene lengths for each gene
-len <- countsDF %>% select(pattern, genelen) %>%
-  mutate(pattern = as.character(pattern)) %>% 
-  group_by(pattern) %>% 
-  summarize(genelen = min(genelen))
-
-# read table with sequencing depth info
-seqs <- read.table("./data/sampleDepth.txt", stringsAsFactors = FALSE, header = TRUE, sep = "\t") %>% 
-  rename(names = name,
-         nreads = length)
-
-# make dataframe from OTU table
-counts <- as.data.frame(nocontrol@otu_table) %>% 
-  rownames_to_column(var = "pattern") %>% 
-  pivot_longer(cols = starts_with("G527_"), names_to = "names", values_to = "count") %>% 
-  # add gene length
-  inner_join(len, by = "pattern")  %>% 
-  # add sequencing depth
-  inner_join(seqs, by = "names")
-
-## NORMALIZE: to gene length and number of reads
-norm <- counts %>% 
-  mutate(
-    # normalize to gene length
-    normgenelen = 
-      case_when(
-        # if there are no genes, keep as 0
-        count == 0 ~ 0,
-        # otherwise, divide by gene length
-        count != 0 ~ count / genelen
-      ),
-          
-    # then normalize by number of reads
-    normseqdepth = 
-      case_when(
-        # if there are no genes, keep as 0
-        count == 0 ~ 0,
-        # otherwise, divide by number of reads
-        count != 0 ~ normgenelen / nreads
-      )
-  ) %>% 
-  # keep only final normalization
-  select(pattern, names, normseqdepth)
+require(ggpubr)
 
 
-# calculate relative abundance
+# non-parametric for one-way ANOVA
 
-# make the dataframe horizontal to calculate relative abundance
-normh <- norm %>% 
-  pivot_wider(names_from = pattern, values_from = normseqdepth, values_fill = list(normseqdepth = 0)) %>% 
-  # decostand() requires a matrix, so make the names into rownames
-  column_to_rownames(var = "names")
+kruskal.test(relabun ~ site, data = counts)
 
-# calculate relative abundance
-relabun <- decostand(normh, method = "range", MARGIN = 2) %>% 
-  # bring back the names column
-  rownames_to_column(var = "name")
+
+# post-hoc for kruskal wallis
+pairwise.wilcox.test(counts$relabun, counts$farm,
+                     p.adjust.method = "bonferroni")
+
+# this is the t test
+wilcox.test(relabun ~ farm, data = counts)
+
+
+## WILCOX RANK SUM FOR COMPARISONS BY FARM
+counts <- counts %>% 
+  mutate(farm = factor(farm, ordered = TRUE, levels = c("1", "2")),
+         animal = factor(animal))
+
+# difference in resistance classes 
+wilcox.test(relabun ~ farm, data = filter(counts, broadclass == "Biocides"))
+boxplot(relabun ~ broadclass, data = counts)
+
+w <- wilcox.test(relabun ~ farm, data = filter(counts, broadclass == "Biocides"))
+
+classes <- unique(counts$broadclass)
+proteins <- unique(counts$protein)
+genes <- unique(counts$gene)
+patterns <- unique(counts$pattern)
+
+outsig <- data.frame()
+for(i in 1:length(patterns)) {
+  
+  w <- wilcox.test(relabun ~ farm, data = filter(counts, pattern == patterns[i]),
+                   exact = FALSE)
+  
+  outsig[i, "pattern"] <- patterns[i]
+  outsig[i, "pval"] <- round(w$p.value, 3)
+  
+  if(w$p.value < 0.05) {
+    outsig[i, "sig"] <- TRUE
+  } else {outsig[i, "sig"] <- FALSE}
+  
+}
+
+ggplot(data = filter(counts, broadclass == "Biocides"), aes(x = farm, y = relabun, group = protein, fill = protein)) +
+  geom_col()
 
