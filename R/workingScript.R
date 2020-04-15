09f1de817b5ea6fb6c2b722a7f1ed9fe337fa323
Emily <epb5360@psu.edu>
2020-04-15 12:07:21 -0400

    work script

--- R/workingScript.R
+++ R/workingScript.R
@@ -16,6 +16,8 @@ pairwise.wilcox.test(counts$relabun, counts$farm,
 wilcox.test(relabun ~ farm, data = counts)
 
 
+# ---- WilcoxRank ----
+
 ## WILCOX RANK SUM FOR COMPARISONS BY FARM
 counts <- counts %>% 
   mutate(farm = factor(farm, ordered = TRUE, levels = c("1", "2")),
@@ -47,21 +49,65 @@ for(i in 1:length(patterns)) {
   
 }
 
+s <- outsig %>% filter(sig == TRUE)
 # get only significant variables and add median + IQR
 sigs <- outsig %>% 
   filter(sig == "TRUE") %>% 
   left_join(counts, by = "pattern") %>% 
-  group_by(farm, pattern, broadclass, type, protein, gene) %>% 
+  group_by(farm, pattern) %>% 
   summarize(median = median(relabun),
             IQR = IQR(relabun)) %>% 
   ungroup() %>% 
   mutate(farm = factor(farm))
 
 
+
+  
+# for genes that are present in both farms, see which farm has more abundance
+sum <- outsig %>% 
+  filter(sig == TRUE) %>% 
+  left_join(counts, by = "pattern")
+
 # visualize the major trends
-ggplot(data = sigs, aes(x = farm, y = median, group = gene)) +
+ggplot(data = sigs, aes(x = farm, y = median, group = pattern)) +
   geom_point() +
+  geom_jitter() +
   geom_line() +
   theme_bw() +
-  ggtitle("Significantly different genes by farm (n = 85)")
+  labs(x = "Farm", y = "Median Relative Abundance") +
+  ggtitle("Significantly different types by farm (n = 18)")
+
+## ---- Kruskal-Wallis ----
+
+## BOTH FARMS
+outsig <- data.frame()
+for(i in 1:length(types)) {
   
+  k <- kruskal.test(relabun ~ site, data = filter(counts, type == types[i]))
+  
+  outsig[i, "type"] <- types[i]
+  outsig[i, "pval"] <- round(k$p.value, 3)
+  
+  if(k$p.value < 0.05) {
+    outsig[i, "sig"] <- TRUE
+  } else {outsig[i, "sig"] <- FALSE}
+  
+}
+
+# get significant variables
+sigs <- outsig %>% filter(sig == "TRUE") %>%  
+  left_join(counts, by = "type") %>% 
+  group_by(site, broadclass, type) %>% 
+  summarize(median = median(relabun),
+            IQR = IQR(relabun)) %>% 
+  ungroup() %>% 
+  mutate(site = factor(site))
+
+# visualize the major trends
+ggplot(data = sigs, aes(x = site, y = median, group = type)) +
+  geom_point() +
+  geom_jitter() +
+  geom_line() +
+  theme_bw() +
+  labs(x = "Body Site", y = "Median Relative Abundance") +
+  ggtitle("Significantly different proteins by site (n = 46)")
